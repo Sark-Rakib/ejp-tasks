3701ee24bb2654ae34990de0e99c646987c0ad83
Sark-Rakib <mdrakibsarkar1@gmail.com>
2025-11-27 14:26:24 +0600

    add

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,16 +1,18 @@
 "use client";
+
 import { AuthContext } from "@/Context/AuthContext";
-import button from "daisyui/components/button";
 import Link from "next/link";
-import React, { useContext } from "react";
+import { useContext } from "react";
 
 export default function Navbar() {
   const { user, logout } = useContext(AuthContext);
+
   return (
     <div className="w-11/12 mx-auto mt-5 sticky top-5 z-50 rounded-full px-5 navbar bg-base-100 shadow-sm">
-      <div className="navbar-start ">
+      {/* Navbar Start */}
+      <div className="navbar-start">
         <div className="dropdown">
-          <div tabIndex={0} role="button" className="lg:hidden">
+          <div tabIndex={0} className="lg:hidden cursor-pointer">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-5 w-5"
@@ -28,29 +30,72 @@ export default function Navbar() {
           </div>
           <ul
             tabIndex="-1"
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-10 mt-3 w-52 p-2 shadow"
           >
-            <Link href="/">Home</Link>
-            <Link href="/about">About</Link>
-            <Link href="/blog">Blog</Link>
-            <Link href="/contact">Contact</Link>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+            <li>
+              <Link href="/blog">Blog</Link>
+            </li>
+            <li>
+              <Link href="/contact">Contact</Link>
+            </li>
           </ul>
         </div>
         <p className="text-xl font-bold px-5">MyShop</p>
       </div>
+
+      {/* Navbar Center */}
       <div className="navbar-center hidden md:flex">
         <ul className="menu menu-horizontal px-1 flex gap-5">
-          <Link href="/">Home</Link>
-          <Link href="/about">About</Link>
-          <Link href="/blog">Blog</Link>
-          <Link href="/contact">Contact</Link>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/blog">Blog</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
         </ul>
       </div>
+
+      {/* Navbar End */}
       <div className="navbar-end">
         {user ? (
-          <button onClick={logout} className="btn">
-            LogOut
-          </button>
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img src={user.photoURL} alt="User Photo" />
+              </div>
+            </label>
+            <ul
+              tabIndex={0}
+              className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <span className="font-bold">
+                  {user.displayName || user.email}
+                </span>
+              </li>
+              <li>
+                <Link href="/add-product">Add Product</Link>
+              </li>
+              <li>
+                <Link href="/manage-products">Manage Products</Link>
+              </li>
+              <li>
+                <button onClick={logout}>Logout</button>
+              </li>
+            </ul>
+          </div>
         ) : (
           <Link href="/login" className="btn">
             Login
